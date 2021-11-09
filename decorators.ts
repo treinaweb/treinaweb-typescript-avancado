2250a27b1eb0474a9314f88f54887d42dcd752d4
wesleygado <wesleygado@gmail.com>
2021-11-09 11:18:51 -0300

    Decorators Funções

--- decorators.ts
+++ decorators.ts
@@ -21,7 +21,7 @@ class Sony{
 let tvSony = new Sony();
 console.log(tvSony); */
 
-function minSize(size: number){
+/*function minSize(size: number){
     return (target: any, key: any) => {
         let val = target[key]
 
@@ -51,4 +51,41 @@ class LG{
 }
 
 let lgTV = new LG(10);
-console.log(lgTV.size);
+console.log(lgTV.size); */
+
+function delay(time: number){
+    return (target: any, key: string, descriptor: PropertyDescriptor) => {
+        let method = descriptor.value;
+
+        descriptor.value = function (...args: any) {
+            setTimeout(() => {
+                method.apply(this, args)
+            }, time);
+        }
+
+        return descriptor;
+    }
+}
+
+
+class Timer {
+
+    @delay(5000)
+    showMsg(){
+        console.log("mensagem após 5 segundos");
+    }
+
+    @delay(500)
+    newTimer(cont: number): number{
+        if (cont === 0 ) {
+            return 0
+        }else {
+            console.log(`Contador em ${cont}`);
+            return this.newTimer(cont - 1);
+        }
+    }
+}
+
+let t = new Timer();
+t.showMsg();
+t.newTimer(5);
