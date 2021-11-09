c3f5090efcf98bf203e3bbb9de1a13e5123e63f9
wesleygado <wesleygado@gmail.com>
2021-11-09 11:16:12 -0300

    Decorators Propriedades

--- decorators.ts
+++ decorators.ts
@@ -1,4 +1,4 @@
-function setOS(os: string){
+/* function setOS(os: string){
     return (target: any) => {
         return class extends target {
             version = os;
@@ -19,4 +19,36 @@ class Sony{
 }
 
 let tvSony = new Sony();
-console.log(tvSony);
+console.log(tvSony); */
+
+function minSize(size: number){
+    return (target: any, key: any) => {
+        let val = target[key]
+
+        const getter = () => val;
+        const setter = (value: number) => {
+            if(value < size){
+                console.log(`O tamanho da tela deve ser maior que ${size} polegadas`);
+            } else {
+                val = value;
+            }
+        }
+
+        Object.defineProperty(target, key, {
+            get: getter,
+            set: setter
+        });
+    }
+}
+
+class LG{
+    @minSize(10)
+    size: number;
+
+    constructor(size: number){
+        this.size = size;
+    }
+}
+
+let lgTV = new LG(10);
+console.log(lgTV.size);
